06da11c50a6e09e82e8d18cf5d44849d0d40226e
Rowan Chander <rowan.chander@itvitaelearning.nl>
2022-01-03 13:20:18 +0100

    Finished a crude mainscreen and calculation

--- UnitConverter/Program.cs
+++ UnitConverter/Program.cs
@@ -37,63 +37,29 @@ namespace UnitConverter
 
             Console.Clear();
 
-            float userMeasurement = 0;
-
             if (input == 1)
             {
-                Console.WriteLine("Please enter a number of meters to be converted to centimeter");
-                try
-                {
-                    float.TryParse(Console.ReadLine(), out userMeasurement);
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Please enter a correct number!");
-                    throw;
-                }
-                Console.WriteLine($"{converterService.MeterToCentimeter(userMeasurement)} centimeter");
-                //Console.ReadKey();
+                MToCm();
             }
             else if (input == 2)
             {
-                Console.WriteLine("Please enter a number of centimeters to be converted to meters");
-                try
-                {
-                    float.TryParse(Console.ReadLine(), out userMeasurement);
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Please enter a correct number!");
-                    throw;
-                }
-                Console.WriteLine($"{converterService.CentimeterToMeter(userMeasurement)} meter");
+                CmToM();
             }
             else if (input == 3)
             {
-
-                Console.WriteLine("Please enter a number of centimeters to be converted to millimeters");
-                try
-                {
-                    float.TryParse(Console.ReadLine(), out userMeasurement);
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Please enter a correct number!");
-                    throw;
-                }
-                Console.WriteLine($"{converterService.CentimeterToMillimeter(userMeasurement)} millimeter");
+                CmToMm();
             }
             else if (input == 4)
             {
-
+                MmToCm();
             }
             else if (input == 5)
             {
-
+                MToIn();
             }
             else if (input == 6)
             {
-
+                InToM();
             }
             else if (input == 7)
             {
@@ -107,6 +73,198 @@ namespace UnitConverter
                 Console.ReadKey();
                 Console.Clear();
             }
+        }
+
+        void CmToM()
+        {
+            Console.WriteLine("Please enter a number of centimeters to be converted to meters");
+            float.TryParse(Console.ReadLine(), out float input);
+            Console.WriteLine($"{converterService.CentimeterToMeter(input)} meter");
+
+            Console.WriteLine("\nPlease select any of the following options and press enter: " +
+    "\n1. Recalculate a different number. " +
+    "\n2. Return to Main Menu. " +
+    "\n3. Kill this program.");
+            int.TryParse(Console.ReadLine(), out int choice);
+            if (choice == 1)
+            {
+                Console.Clear();
+                CmToM();
+            }
+            else if (choice == 2)
+            {
+
+            }
+            else if (choice == 3)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Choose a valid option.");
+            }
+
+            Console.Clear();
+        }
+
+        void MToCm()
+        {
+            Console.WriteLine("Please enter a number of meters to be converted to centimeter");
+            float.TryParse(Console.ReadLine(), out float input);
+            Console.WriteLine($"{converterService.MeterToCentimeter(input)} centimeter");
+
+            Console.WriteLine("\nPlease select any of the following options and press enter: " +
+"\n1. Recalculate a different number. " +
+"\n2. Return to Main Menu. " +
+"\n3. Kill this program.");
+            int.TryParse(Console.ReadLine(), out int choice);
+            if (choice == 1)
+            {
+                Console.Clear();
+                MToCm();
+            }
+            else if (choice == 2)
+            {
+
+            }
+            else if (choice == 3)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Choose a valid option.");
+            }
+
+            Console.Clear();
+        }
+
+        void CmToMm()
+        {
+            Console.WriteLine("Please enter a number of centimeters to be converted to millimeters");
+            float.TryParse(Console.ReadLine(), out float input);
+            Console.WriteLine($"{converterService.CentimeterToMillimeter(input)} millimeter");
+
+            Console.WriteLine("\nPlease select any of the following options and press enter: " +
+"\n1. Recalculate a different number. " +
+"\n2. Return to Main Menu. " +
+"\n3. Kill this program.");
+            int.TryParse(Console.ReadLine(), out int choice);
+            if (choice == 1)
+            {
+                Console.Clear();
+                CmToMm();
+            }
+            else if (choice == 2)
+            {
+
+            }
+            else if (choice == 3)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Choose a valid option.");
+            }
+
+            Console.Clear();
+        }
+
+        void MmToCm()
+        {
+            Console.WriteLine("Please enter a number of millimeters to be converted to centimeters");
+            float.TryParse(Console.ReadLine(), out float input);
+            Console.WriteLine($"{converterService.MillimeterToCentimeter(input)} centimeter");
+
+            Console.WriteLine("\nPlease select any of the following options and press enter: " +
+"\n1. Recalculate a different number. " +
+"\n2. Return to Main Menu. " +
+"\n3. Kill this program.");
+            int.TryParse(Console.ReadLine(), out int choice);
+            if (choice == 1)
+            {
+                Console.Clear();
+                MmToCm();
+            }
+            else if (choice == 2)
+            {
+
+            }
+            else if (choice == 3)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Choose a valid option.");
+            }
+
+            Console.Clear();
+        }
+
+        void MToIn()
+        {
+            Console.WriteLine("Please enter a number of meters to be converted to inches");
+            float.TryParse(Console.ReadLine(), out float input);
+            Console.WriteLine($"{converterService.MeterToInch(input)} inch");
+
+            Console.WriteLine("\nPlease select any of the following options and press enter: " +
+"\n1. Recalculate a different number. " +
+"\n2. Return to Main Menu. " +
+"\n3. Kill this program.");
+            int.TryParse(Console.ReadLine(), out int choice);
+            if (choice == 1)
+            {
+                Console.Clear();
+                MToIn();
+            }
+            else if (choice == 2)
+            {
+
+            }
+            else if (choice == 3)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Choose a valid option.");
+            }
+
+            Console.Clear();
+        }
+
+        void InToM()
+        {
+            Console.WriteLine("Please enter a number of inches to be converted to meters");
+            float.TryParse(Console.ReadLine(), out float input);
+            Console.WriteLine($"{converterService.InchToMeter(input)} meter");
+
+            Console.WriteLine("\nPlease select any of the following options and press enter: " +
+"\n1. Recalculate a different number. " +
+"\n2. Return to Main Menu. " +
+"\n3. Kill this program.");
+            int.TryParse(Console.ReadLine(), out int choice);
+            if (choice == 1)
+            {
+                Console.Clear();
+                InToM();
+            }
+            else if (choice == 2)
+            {
+
+            }
+            else if (choice == 3)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Choose a valid option.");
+            }
+
+            Console.Clear();
         }
     }
 }
