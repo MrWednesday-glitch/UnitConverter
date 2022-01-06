2b267ec047470a322daa5aa7342041de36b5bf5a
Rowan Chander <rowan.chander@itvitaelearning.nl>
2022-01-06 13:17:52 +0100

    Merged the main form and subform(s). Thus removing the need for subforms.

--- UnitConverter.Winforms/Overview.Designer.cs
+++ UnitConverter.Winforms/Overview.Designer.cs
@@ -36,6 +36,13 @@
             this.button_MillimeterToCentimeter = new System.Windows.Forms.Button();
             this.button_MeterToInch = new System.Windows.Forms.Button();
             this.button_InchToMeter = new System.Windows.Forms.Button();
+            this.button_Reset = new System.Windows.Forms.Button();
+            this.textBox_Input = new System.Windows.Forms.TextBox();
+            this.label_Output = new System.Windows.Forms.Label();
+            this.label_InputType = new System.Windows.Forms.Label();
+            this.label_OutputType = new System.Windows.Forms.Label();
+            this.label_ENterANumberToBeConverted = new System.Windows.Forms.Label();
+            this.label_ConvertedNumber = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label_ConverterOverview
@@ -43,7 +50,7 @@
             this.label_ConverterOverview.AutoSize = true;
             this.label_ConverterOverview.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label_ConverterOverview.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label_ConverterOverview.Location = new System.Drawing.Point(39, 24);
+            this.label_ConverterOverview.Location = new System.Drawing.Point(257, 23);
             this.label_ConverterOverview.Name = "label_ConverterOverview";
             this.label_ConverterOverview.Size = new System.Drawing.Size(192, 27);
             this.label_ConverterOverview.TabIndex = 0;
@@ -52,7 +59,7 @@
             // button_MeterToCentimeter
             // 
             this.button_MeterToCentimeter.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button_MeterToCentimeter.Location = new System.Drawing.Point(12, 76);
+            this.button_MeterToCentimeter.Location = new System.Drawing.Point(12, 71);
             this.button_MeterToCentimeter.Name = "button_MeterToCentimeter";
             this.button_MeterToCentimeter.Size = new System.Drawing.Size(112, 52);
             this.button_MeterToCentimeter.TabIndex = 1;
@@ -63,7 +70,7 @@
             // button_Exit
             // 
             this.button_Exit.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button_Exit.Location = new System.Drawing.Point(97, 278);
+            this.button_Exit.Location = new System.Drawing.Point(591, 250);
             this.button_Exit.Name = "button_Exit";
             this.button_Exit.Size = new System.Drawing.Size(82, 52);
             this.button_Exit.TabIndex = 2;
@@ -74,58 +81,137 @@
             // button_CentimeterToMeter
             // 
             this.button_CentimeterToMeter.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button_CentimeterToMeter.Location = new System.Drawing.Point(142, 76);
+            this.button_CentimeterToMeter.Location = new System.Drawing.Point(130, 71);
             this.button_CentimeterToMeter.Name = "button_CentimeterToMeter";
             this.button_CentimeterToMeter.Size = new System.Drawing.Size(106, 52);
             this.button_CentimeterToMeter.TabIndex = 3;
             this.button_CentimeterToMeter.Text = "Centimeter ==> Meter";
             this.button_CentimeterToMeter.UseVisualStyleBackColor = true;
+            this.button_CentimeterToMeter.Click += new System.EventHandler(this.button_CentimeterToMeter_Click);
             // 
             // button_CentimeterToMillimeter
             // 
             this.button_CentimeterToMillimeter.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button_CentimeterToMillimeter.Location = new System.Drawing.Point(12, 134);
+            this.button_CentimeterToMillimeter.Location = new System.Drawing.Point(12, 129);
             this.button_CentimeterToMillimeter.Name = "button_CentimeterToMillimeter";
-            this.button_CentimeterToMillimeter.Size = new System.Drawing.Size(112, 65);
+            this.button_CentimeterToMillimeter.Size = new System.Drawing.Size(112, 52);
             this.button_CentimeterToMillimeter.TabIndex = 4;
             this.button_CentimeterToMillimeter.Text = "Centimeter ==> Millimeter";
             this.button_CentimeterToMillimeter.UseVisualStyleBackColor = true;
+            this.button_CentimeterToMillimeter.Click += new System.EventHandler(this.button_CentimeterToMillimeter_Click);
             // 
             // button_MillimeterToCentimeter
             // 
             this.button_MillimeterToCentimeter.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button_MillimeterToCentimeter.Location = new System.Drawing.Point(142, 135);
+            this.button_MillimeterToCentimeter.Location = new System.Drawing.Point(130, 129);
             this.button_MillimeterToCentimeter.Name = "button_MillimeterToCentimeter";
-            this.button_MillimeterToCentimeter.Size = new System.Drawing.Size(106, 64);
+            this.button_MillimeterToCentimeter.Size = new System.Drawing.Size(106, 52);
             this.button_MillimeterToCentimeter.TabIndex = 5;
             this.button_MillimeterToCentimeter.Text = "Millimeter ==> Centimeter";
             this.button_MillimeterToCentimeter.UseVisualStyleBackColor = true;
+            this.button_MillimeterToCentimeter.Click += new System.EventHandler(this.button_MillimeterToCentimeter_Click);
             // 
             // button_MeterToInch
             // 
             this.button_MeterToInch.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button_MeterToInch.Location = new System.Drawing.Point(12, 205);
+            this.button_MeterToInch.Location = new System.Drawing.Point(12, 187);
             this.button_MeterToInch.Name = "button_MeterToInch";
-            this.button_MeterToInch.Size = new System.Drawing.Size(112, 55);
+            this.button_MeterToInch.Size = new System.Drawing.Size(112, 52);
             this.button_MeterToInch.TabIndex = 6;
             this.button_MeterToInch.Text = "Meter ==> Inch";
             this.button_MeterToInch.UseVisualStyleBackColor = true;
+            this.button_MeterToInch.Click += new System.EventHandler(this.button_MeterToInch_Click);
             // 
             // button_InchToMeter
             // 
             this.button_InchToMeter.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button_InchToMeter.Location = new System.Drawing.Point(142, 205);
+            this.button_InchToMeter.Location = new System.Drawing.Point(130, 187);
             this.button_InchToMeter.Name = "button_InchToMeter";
-            this.button_InchToMeter.Size = new System.Drawing.Size(106, 55);
+            this.button_InchToMeter.Size = new System.Drawing.Size(106, 52);
             this.button_InchToMeter.TabIndex = 7;
             this.button_InchToMeter.Text = "Inch ==> Meter";
             this.button_InchToMeter.UseVisualStyleBackColor = true;
+            this.button_InchToMeter.Click += new System.EventHandler(this.button_InchToMeter_Click);
+            // 
+            // button_Reset
+            // 
+            this.button_Reset.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_Reset.Location = new System.Drawing.Point(501, 250);
+            this.button_Reset.Name = "button_Reset";
+            this.button_Reset.Size = new System.Drawing.Size(84, 53);
+            this.button_Reset.TabIndex = 8;
+            this.button_Reset.Text = "Reset";
+            this.button_Reset.UseVisualStyleBackColor = true;
+            this.button_Reset.Click += new System.EventHandler(this.button_Reset_Click);
+            // 
+            // textBox_Input
+            // 
+            this.textBox_Input.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox_Input.Location = new System.Drawing.Point(462, 85);
+            this.textBox_Input.Name = "textBox_Input";
+            this.textBox_Input.PlaceholderText = "0";
+            this.textBox_Input.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox_Input.Size = new System.Drawing.Size(157, 26);
+            this.textBox_Input.TabIndex = 9;
+            // 
+            // label_Output
+            // 
+            this.label_Output.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_Output.Location = new System.Drawing.Point(462, 204);
+            this.label_Output.Name = "label_Output";
+            this.label_Output.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label_Output.Size = new System.Drawing.Size(157, 18);
+            this.label_Output.TabIndex = 10;
+            this.label_Output.Text = "0";
+            // 
+            // label_InputType
+            // 
+            this.label_InputType.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_InputType.Location = new System.Drawing.Point(625, 85);
+            this.label_InputType.Name = "label_InputType";
+            this.label_InputType.Size = new System.Drawing.Size(46, 26);
+            this.label_InputType.TabIndex = 11;
+            // 
+            // label_OutputType
+            // 
+            this.label_OutputType.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_OutputType.Location = new System.Drawing.Point(625, 204);
+            this.label_OutputType.Name = "label_OutputType";
+            this.label_OutputType.Size = new System.Drawing.Size(46, 18);
+            this.label_OutputType.TabIndex = 12;
+            // 
+            // label_ENterANumberToBeConverted
+            // 
+            this.label_ENterANumberToBeConverted.AutoSize = true;
+            this.label_ENterANumberToBeConverted.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_ENterANumberToBeConverted.Location = new System.Drawing.Point(257, 88);
+            this.label_ENterANumberToBeConverted.Name = "label_ENterANumberToBeConverted";
+            this.label_ENterANumberToBeConverted.Size = new System.Drawing.Size(199, 18);
+            this.label_ENterANumberToBeConverted.TabIndex = 13;
+            this.label_ENterANumberToBeConverted.Text = "Enter a number to be converted";
+            // 
+            // label_ConvertedNumber
+            // 
+            this.label_ConvertedNumber.AutoSize = true;
+            this.label_ConvertedNumber.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_ConvertedNumber.Location = new System.Drawing.Point(257, 204);
+            this.label_ConvertedNumber.Name = "label_ConvertedNumber";
+            this.label_ConvertedNumber.Size = new System.Drawing.Size(117, 18);
+            this.label_ConvertedNumber.TabIndex = 14;
+            this.label_ConvertedNumber.Text = "Converted number";
             // 
             // Overview
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(272, 364);
+            this.ClientSize = new System.Drawing.Size(685, 314);
+            this.Controls.Add(this.label_ConvertedNumber);
+            this.Controls.Add(this.label_ENterANumberToBeConverted);
+            this.Controls.Add(this.label_OutputType);
+            this.Controls.Add(this.label_InputType);
+            this.Controls.Add(this.label_Output);
+            this.Controls.Add(this.textBox_Input);
+            this.Controls.Add(this.button_Reset);
             this.Controls.Add(this.button_InchToMeter);
             this.Controls.Add(this.button_MeterToInch);
             this.Controls.Add(this.button_MillimeterToCentimeter);
@@ -151,5 +237,12 @@
         private System.Windows.Forms.Button button_MillimeterToCentimeter;
         private System.Windows.Forms.Button button_MeterToInch;
         private System.Windows.Forms.Button button_InchToMeter;
+        private System.Windows.Forms.Button button_Reset;
+        private System.Windows.Forms.TextBox textBox_Input;
+        private System.Windows.Forms.Label label_Output;
+        private System.Windows.Forms.Label label_InputType;
+        private System.Windows.Forms.Label label_OutputType;
+        private System.Windows.Forms.Label label_ENterANumberToBeConverted;
+        private System.Windows.Forms.Label label_ConvertedNumber;
     }
 }
--- UnitConverter.Winforms/Overview.cs
+++ UnitConverter.Winforms/Overview.cs
@@ -1,33 +1,127 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using UnitConverter.Winforms.Subforms;
+using UnitConverter.Business;
+using UnitConverter.Common;
 
 namespace UnitConverter.Winforms
 {//TODO make 5 more subforms for the other buttons
     public partial class Overview : Form
     {
+        private readonly ConverterService _converterService;
+        private readonly Logging _logging;
+
         public Overview()
         {
             InitializeComponent();
+            _converterService = new ConverterService();
+            _logging = new Logging();
         }
 
         private void button_MeterToCentimeter_Click(object sender, EventArgs e)
         {
-            MeterToCentimeter formMToCm = new MeterToCentimeter();
-            formMToCm.ShowDialog();
-            //this.Hide();
+            bool success = double.TryParse(textBox_Input.Text, out double input);
+            if (!success)
+            {
+                string errorMessage = "Error: You did not enter a valid number, please try again.";
+                _logging.WriteToLogFile(errorMessage);
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            double output = _converterService.MeterToCentimeter(input);
+            _logging.WriteToLogFile($"{input} meter to {output}.");
+            label_Output.Text = output.ToString();
+            label_InputType.Text = "m";
+            label_OutputType.Text = "cm";
         }
 
         private void button_Exit_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void button_Reset_Click(object sender, EventArgs e)
+        {
+            textBox_Input.Clear();
+            label_Output.Text = "0";
+        }
+
+        private void button_CentimeterToMeter_Click(object sender, EventArgs e)
+        {
+            bool success = double.TryParse(textBox_Input.Text, out double input);
+            if (!success)
+            {
+                string errorMessage = "Error: You did not enter a valid number, please try again.";
+                _logging.WriteToLogFile(errorMessage);
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            double output = _converterService.CentimeterToMeter(input);
+            _logging.WriteToLogFile($"{input} centimeter to {output}.");
+            label_Output.Text = output.ToString();
+            label_InputType.Text = "cm";
+            label_OutputType.Text = "m";
+        }
+
+        private void button_CentimeterToMillimeter_Click(object sender, EventArgs e)
+        {
+            bool success = double.TryParse(textBox_Input.Text, out double input);
+            if (!success)
+            {
+                string errorMessage = "Error: You did not enter a valid number, please try again.";
+                _logging.WriteToLogFile(errorMessage);
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            double output = _converterService.CentimeterToMillimeter(input);
+            _logging.WriteToLogFile($"{input} centimeter to {output}.");
+            label_Output.Text = output.ToString();
+            label_InputType.Text = "cm";
+            label_OutputType.Text = "mm";
+        }
+
+        private void button_MillimeterToCentimeter_Click(object sender, EventArgs e)
+        {
+            bool success = double.TryParse(textBox_Input.Text, out double input);
+            if (!success)
+            {
+                string errorMessage = "Error: You did not enter a valid number, please try again.";
+                _logging.WriteToLogFile(errorMessage);
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            double output = _converterService.MillimeterToCentimeter(input);
+            _logging.WriteToLogFile($"{input} millimeter to {output}.");
+            label_Output.Text = output.ToString();
+            label_InputType.Text = "mm";
+            label_OutputType.Text = "cm";
+        }
+
+        private void button_MeterToInch_Click(object sender, EventArgs e)
+        {
+            bool success = double.TryParse(textBox_Input.Text, out double input);
+            if (!success)
+            {
+                string errorMessage = "Error: You did not enter a valid number, please try again.";
+                _logging.WriteToLogFile(errorMessage);
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            double output = _converterService.MeterToInch(input);
+            _logging.WriteToLogFile($"{input} meter to {output}.");
+            label_Output.Text = output.ToString();
+            label_InputType.Text = "m";
+            label_OutputType.Text = "in";
+        }
+
+        private void button_InchToMeter_Click(object sender, EventArgs e)
+        {
+            bool success = double.TryParse(textBox_Input.Text, out double input);
+            if (!success)
+            {
+                string errorMessage = "Error: You did not enter a valid number, please try again.";
+                _logging.WriteToLogFile(errorMessage);
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            double output = _converterService.InchToMeter(input);
+            _logging.WriteToLogFile($"{input} inch to {output}.");
+            label_Output.Text = output.ToString();
+            label_InputType.Text = "in";
+            label_OutputType.Text = "m";
         }
     }
 }
