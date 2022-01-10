133d7321ceabed7cb4ef379c176ba4ab6105a543
Rowan Chander <rowan.chander@itvitaelearning.nl>
2022-01-10 17:22:22 +0100

    Trying my hand at WPF... I don't get it.

--- UnitConverter.WPF/MainWindow.xaml.cs
+++ UnitConverter.WPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UnitConverter.Business;
 
 namespace UnitConverter.WPF
 {
@@ -20,9 +21,31 @@ namespace UnitConverter.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ConverterService _converterService;
+        private readonly LoggingService _loggingService;
+
         public MainWindow()
         {
             InitializeComponent();
+            _converterService = new ConverterService();
+            _loggingService = new LoggingService();
+        }
+
+        private void Convert_Button_Click(object sender, RoutedEventArgs e)
+        { //It does not get here...?
+            if (MeterToCentimeter_Radio.IsChecked == true)
+            {
+                bool success = double.TryParse(Input_TextBox.Text, out double input);
+                if (!success)
+                {
+                    string errorMessage = "Error: You did not enter a valid number, please try again.";
+                    _loggingService.WriteToDatabase(errorMessage);
+
+                }
+                double output = _converterService.MeterToCentimeter(input);
+                _loggingService.WriteToDatabase($"{input} meter to {output}.");
+                Output_TextBox.Text = output.ToString();
+            }
         }
     }
 }
