d7769e02e12acfbe07b90d63400efeb4342405fd
Rowan Chander <rowan.chander@itvitaelearning.nl>
2022-01-11 15:21:53 +0100

    Still don't get moq. Give up for now.

--- UnitConverter.Business.Test/LoggingServiceTests/WriteToLogfileTests.cs
+++ UnitConverter.Business.Test/LoggingServiceTests/WriteToLogfileTests.cs
@@ -10,6 +10,18 @@ namespace UnitConverter.Business.Test.LoggingServiceTests
 {
     public class WriteToLogfileTests
     {
+        private readonly LoggingService _logging;
+
+        public WriteToLogfileTests()
+        {
+            _logging = new LoggingService();
+        }
+
+        [Fact]
+        public void Should_WriteExpectedOutput()
+        {
+
+        }
         //I still don't really understand mocking so I am not sure how to test this
     }
 }
